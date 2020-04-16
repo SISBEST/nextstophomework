a8d758771e2f1383907250271608ef74775ff1ed
Samuel Sharp <samuelsharp4@gmail.com>
2020-04-16 08:38:31 -0700

    Set up test notification. Minor input stylesheet changes.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const {
   app,
   BrowserWindow,
-  Menu
+  Menu,
+  Notification
 } = require('electron');
 var mainWindow;
 const rookout = require('rookout');
 rookout.start({
   token: 'f42170bc7356fdde367e6c2fce185e2ec1b3ebfd942ecfe0ab8165e590d85c2f'
-})
-
+});
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -18,8 +18,27 @@ function createWindow() {
     }
   });
   mainWindow.loadFile('login.html');
-
   mainWindow.webContents.openDevTools()
+
+  if (Notification.isSupported()) {
+    const notification = new Notification({
+      title: 'Next Stop: Homework!',
+      subtitle: 'TEST',
+      body: 'TEST',
+      hasReply: true
+    })
+
+    notification.on('show', () => console.log('Test Notification Shown'))
+    notification.on('click', () => console.log('Test Notification Clicked'))
+    notification.on('close', () => console.log('Test Notification Closed'))
+    notification.on('reply', (event, reply) => {
+      console.log(`Test Reply: ${reply}`)
+    })
+
+    notification.show()
+  } else {
+    window.alert('Notifications are not supported on this device.')
+  }
 }
 app.on('ready', createWindow);
 app.on('window-all-closed', function() {
